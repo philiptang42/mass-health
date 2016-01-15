44c007eff9596312a1001a7ecbbed37519522f5c
philiptang42 <philiptang42@gmail.com>
2016-01-14 22:18:23 -0500

    inserting CSV into SQL via import.rb

--- import.rb
+++ import.rb
@@ -27,7 +27,12 @@ def insert_records(town, formatted_numbers)
     town["Per Capita income, year 2000"],
     formatted_numbers[:persons_below_200],
     town["% all Persons Living Below 200% Poverty Level, year 2000"],
-    
-
-
+    town["% adequacy prenatal care (kotelchuck)"],
+    town["% C-section deliveries, 2005-2008"],
+    formatted_numbers[:infant_deaths],
+    town["Infant mortality rate (deaths per 1000 live births), 2005-2008"],
+    town["% low birthweight 2005-2008"],
+    town["% multiple births, 2005-2008"],
+    town["% publicly financed prenatal care, 2005-2008"],
+    town["% teen births, 2005-2008"]
   ]
