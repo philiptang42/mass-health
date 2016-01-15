2fa2d1c4dac5508de900ce065e540946c9b117d9
philiptang42 <philiptang42@gmail.com>
2016-01-14 21:55:42 -0500

    inserting CSV into SQL via import.rb

--- import.rb
+++ import.rb
@@ -1,2 +1,25 @@
 require 'csv'
+require 'csv'
 require 'pg'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname = "mass_health")
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+def to_integer(number)
+  delete_comma = number.delete(",")
+  integer = delete_comma.to_i
+  integer
+end
+
+def insert_records(town, formatted_info)
+
+  record = [
+    town['Geography'],
+    
+  ]
--- schema.sql
+++ schema.sql
@@ -1,19 +1,19 @@
 -- DEFINE YOUR DATABASE SCHEMA HERE
-CREATE TABLE mass_health (
+CREATE TABLE town_health_records (
   id serial PRIMARY KEY,
   geography varchar(255),
   total_population_in_2005 integer,
-  age_0-19_in_2005 integer,
-  age_65+_in_2005 integer,
-  per_capita_income_in_2000 currency varchar(255),
-  persons_below_200%_poverty_in_2000 varchar(255),
-  %_of_all_persons_below_200%_poverty_in_2000 varchar(255),
-  %_adequacy_prenatal_care varchar(255),
-  %_C-section_deliveries_2005_to_2008 varchar(255),
+  age_0_to_19_in_2005 integer,
+  age_65_plus_in_2005 integer,
+  per_capita_income_in_2000 varchar(255),
+  persons_below_200_percent_poverty_in_2000 varchar(255),
+  percentage_of_all_persons_below_200_poverty_in_2000 varchar(255),
+  percentage_adequacy_prenatal_care varchar(255),
+  percentage_C_section_deliveries_2005_to_2008 varchar(255),
   infant_deaths_2005_to_2008 varchar(255),
   infant_mortality_rate_2005_to_2008 varchar(255),
-  %_low_birthrate_2005_to_2008 varchar(255),
-  %_multiple_births_2005_to_2008 varchar(255),
-  %_publically_financed_prenatal_care_2005_to_2008 varchar(255),
-  %_teen_births_2005_to_2008 varchar(255)
+  percentage_low_birthrate_2005_to_2008 varchar(255),
+  percentage_multiple_births_2005_to_2008 varchar(255),
+  percentage_publically_financed_prenatal_care_2005_to_2008 varchar(255),
+  percentage_teen_births_2005_to_2008 varchar(255)
 );
